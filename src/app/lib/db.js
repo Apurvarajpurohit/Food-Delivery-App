55613fe10fccb2bd8aadf0ff99ece2ba61a29fc4
Apurvarajpurohit <apurvarajpurohit03@gmail.com>
2025-01-26 21:34:49 +0530

    updated named issue

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,1 @@
-// console.log(username,password,hello)
 export const connectionStr="mongodb+srv://apurvarajpurohit:apurva22J@cluster0.t9278.mongodb.net/restoDB?retryWrites=true&w=majority&appName=Cluster0";
--- src/app/restaurant/page.js
+++ src/app/restaurant/page.js
@@ -1,10 +1,10 @@
 'use client'
 import { useState } from "react";
+import RestaurantLogin from "../_components/RestaurantLogin";
+import RestaurantSignUp from "../_components/RestaurantSignUp";
 import RestaurantHeader from "../_components/RestaurantHeader";
 import Footer from "../_components/Footer";
 import './style.css'
-import RestaurantLogin from "../_components/restaurantLogin";
-import RestaurantSignUp from "../_components/restaurantSignUp";
 
 const Restaurant = () => {
     const [login, setLogin] = useState(true)
